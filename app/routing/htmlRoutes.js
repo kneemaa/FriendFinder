33ecd5806be088b3d0e344266e0da140a1fc098c
Nema Darban <nema.darban@gmail.com>
2018-02-28 22:33:37 -0800

    routing in place

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,11 +1,13 @@
-const express = require('express');
-const app = express();
 const path = require('path');
 
-app.get('/', (req,res) => {
-	res.sendFile(path.join(__dirname,"./app/public/home.html"));
-})
 
-app.get('/survey', (req,res) => {
-	res.sendFile(path.join(__dirname,"./app/public/survey.html"));
+module.exports = ((app) => {
+	app.get('/', (req,res) => {
+		res.sendFile(path.join(__dirname,"../public/home.html"));
+	})
+
+	app.get('/survey', (req,res) => {
+		res.sendFile(path.join(__dirname,"../public/survey.html"));
+	})
+
 })
