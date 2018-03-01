e89cd36026db697debc87143c0cf8300b9e1bcb1
Nema Darban <nema.darban@gmail.com>
2018-02-28 22:35:50 -0800

    importing routes and api

--- server.js
+++ server.js
@@ -4,19 +4,18 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const PORT = process.env.PORT || 3000;
 
+
 const apiRoutes = require('./app/routing/apiRoutes');
 const htmlRoutes = require('./app/routing/htmlRoutes');
 const friends = require('./app/data/friends');
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(bodyParser.json());
 
-app.use(function (req, res) {
-  res.setHeader('Content-Type', 'text/plain')
-  res.write('you posted:\n')
-  res.end(JSON.stringify(req.body, null, 2))
-})
+require('./app/routing/apiRoutes.js')(app);
+require('./app/routing/htmlRoutes.js')(app);
 
 app.listen(PORT, () => {
 	console.log(`App listening on PORT ${PORT}`);
